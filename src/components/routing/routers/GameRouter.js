0ccdfa867f45d070bc966dc0cd2ac7d3df30b174
B1s9l <furrerbasil@gmail.com>
2024-03-28 00:37:44 +0100

    Added routing towards teamdashboard
    sopra-fs24-group07/sopra-fs24-group-07-client#6
    - Added routing towards individual team dashboards

--- src/components/routing/routers/GameRouter.js
+++ src/components/routing/routers/GameRouter.js
@@ -4,17 +4,15 @@ import Game from "../../views/Game";
 import TeamsOverview from "../../views/TeamsOverview";
 import PropTypes from "prop-types";
 import Header from "../../views/Header";
+import TeamDashboard from "../../views/TeamDashboard";
 
-const GameRouter = () => {
+const GameRouter = ({ base }) => {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <Header height="100" />
       <Routes>
         <Route path="" element={<TeamsOverview />} />
-
-        <Route path="teams" element={<TeamsOverview />} />
-
-        <Route path="*" element={<Navigate to="teams" replace />} />
+        <Route path=":teamId" element={<TeamDashboard />} />
       </Routes>
     </div>
   );
