7f38c19c05469ffa5abd53450c37e1f65d2255b1
B1s9l <furrerbasil@gmail.com>
2024-03-27 20:09:08 +0100

    Created TeamsOverview scss file
    sopra-fs24-group07/sopra-fs24-group-07-client#5
    - Created the scss file for the TeamsOverview page

---
+++ src/components/views/TeamsOverview.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { api, handleError } from "helpers/api";
+import User from "models/User";
+import { useNavigate } from "react-router-dom";
+import { Button } from "components/ui/Button";
+import "styles/views/TeamsOverview.scss";
+import BaseContainer from "components/ui/BaseContainer";
+import PropTypes from "prop-types";
+
+const TeamsOverview = () => {};
