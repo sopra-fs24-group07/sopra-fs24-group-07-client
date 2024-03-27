f005f002348b0716f35511a307d98712ec29a117
B1s9l <furrerbasil@gmail.com>
2024-03-27 19:21:34 +0100

    Added routing to Register
    sopra-fs24-group07/sopra-fs24-group-07-client#1
    - Added routing for /register
    - Changed domain to /start from /landing

--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -4,6 +4,7 @@ import { GameGuard } from "../routeProtectors/GameGuard";
 import GameRouter from "./GameRouter";
 import { LoginGuard } from "../routeProtectors/LoginGuard";
 import Login from "../../views/Login";
+import Registration from "../../views/Registration";
 import Landing from "../../views/Landing";
 
 /**
@@ -23,12 +24,16 @@ const AppRouter = () => {
           <Route path="/game/*" element={<GameRouter base="/game" />} />
         </Route>
 
+        <Route path="/register" element={<LoginGuard />}>
+          <Route path="/register" element={<Registration />} />
+        </Route>
+
         <Route path="/login" element={<LoginGuard />}>
           <Route path="/login" element={<Login />} />
         </Route>
 
-        <Route path="/landing" element={<LoginGuard />}>
-          <Route path="/landing" element={<Landing />} />
+        <Route path="/start" element={<LoginGuard />}>
+          <Route path="/start" element={<Landing />} />
         </Route>
 
         <Route path="/" element={<Navigate to="/game" replace />} />
