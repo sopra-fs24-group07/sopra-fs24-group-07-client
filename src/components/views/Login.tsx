a84d71cc2238e8e2170656662cc9d61830b4364a
B1s9l <furrerbasil@gmail.com>
2024-03-27 19:49:59 +0100

    Implemented username and password input
    sopra-fs24-group07/sopra-fs24-group-07-client#3
    - Implemented input fields for username and password required for login

--- src/components/views/Login.tsx
+++ src/components/views/Login.tsx
@@ -35,12 +35,12 @@ FormField.propTypes = {
 
 const Login = () => {
   const navigate = useNavigate();
-  const [name, setName] = useState<string>(null);
   const [username, setUsername] = useState<string>(null);
+  const [password, setPassword] = useState<string>(null);
 
   const doLogin = async () => {
     try {
-      const requestBody = JSON.stringify({ username, name });
+      const requestBody = JSON.stringify({ username, password });
       const response = await api.post("/users", requestBody);
 
       // Get the returned user and update a new object.
@@ -69,10 +69,14 @@ const Login = () => {
             value={username}
             onChange={(un: string) => setUsername(un)}
           />
-          <FormField label="Name" value={name} onChange={(n) => setName(n)} />
+          <FormField
+            label="Password"
+            value={password}
+            onChange={(n) => setPassword(n)}
+          />
           <div className="login button-container">
             <Button
-              disabled={!username || !name}
+              disabled={!username || !password}
               width="50%"
               onClick={() => doLogin()}
             >
