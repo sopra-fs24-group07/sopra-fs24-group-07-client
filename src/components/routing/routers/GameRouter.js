293bac8118a9ba62654811763b9f98d6503cb639
B1s9l <furrerbasil@gmail.com>
2024-03-27 10:46:49 +0100

    Moved header to GameRouter: not wanted on landing page

--- src/components/routing/routers/GameRouter.js
+++ src/components/routing/routers/GameRouter.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Game from "../../views/Game";
 import PropTypes from "prop-types";
+import Header from "../../views/Header";
 
 const GameRouter = () => {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
+      <Header height="100" />
       <Routes>
         <Route path="" element={<Game />} />
 
