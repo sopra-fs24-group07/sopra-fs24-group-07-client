aa3058b168456446618ab442b7a27b4f48d1ff1c
B1s9l <furrerbasil@gmail.com>
2024-03-28 01:01:21 +0100

    Changed the title displayed on this page
    sopra-fs24-group07/sopra-fs24-group-07-client#17
    - Changed the title slightly

---
+++ src/components/views/Profile.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "styles/views/TeamsOverview.scss";
+import BaseContainer from "components/ui/BaseContainer";
+
+const Profile = () => {
+  return (
+    <BaseContainer>
+      <div className="profile">
+        <h2>This is the user profile</h2>
+      </div>
+    </BaseContainer>
+  );
+};
+
+export default Profile;
