43344291214d3ae41e2b5fdfe17bd87f448cd226
B1s9l <furrerbasil@gmail.com>
2024-03-29 12:05:09 +0100

    Changed the implementation of the clickable logo
    sopra-fs24-group07/sopra-fs24-group-07-client#3
    - Changed the implementation of the clickable logo
    - Removed the button
    - Made the logo itself clickable

--- src/components/views/Landing.tsx
+++ src/components/views/Landing.tsx
@@ -1,33 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Button } from "components/ui/Button";
-import "styles/views/Login.scss";
-import BaseContainer from "components/ui/BaseContainer";
+import "styles/views/Landing.scss";
 import logo from "../../logo.png";
 
 const Landing = () => {
   const navigate = useNavigate();
 
-  const doContinue = () => {
+  const handleLogoClick = () => {
     navigate("/login");
   };
 
   return (
-    <BaseContainer>
-      <div className="login container">
-        <div className="logo container">
-          <Button onClick={doContinue}>
-            <img src={logo} alt="Logo" />
-          </Button>
-        </div>
+    <div className="landing container">
+      <div className="landing logo" onClick={handleLogoClick}>
+        <img src={logo} alt="Logo" />
       </div>
-      <p
-        className="login text"
-        style={{ textAlign: "center", marginTop: "0.5em" }}
-      >
-        {"<<Click the logo to continue!>>"}
+      <p className="landing click-text">
+        {" "}
+        {"<<Click the logo to continue!>>"}{" "}
       </p>
-    </BaseContainer>
+    </div>
   );
 };
 
