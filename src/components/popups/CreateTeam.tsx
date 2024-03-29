d132ac540dfdeb6985a3db739f32a75279eebf70
B1s9l <furrerbasil@gmail.com>
2024-03-29 16:02:37 +0100

    Updated the createteam popup
    sopra-fs24-group07/sopra-fs24-group-07-client#5
    - Updated the popup
    - Can now input a name

--- src/components/popups/CreateTeam.tsx
+++ src/components/popups/CreateTeam.tsx
@@ -1,26 +1,52 @@
-import React from "react";
-import "../../styles/popups/ProfileMenu.scss";
+import React, { useState } from "react";
+import "../../styles/popups/CreateTeam.scss";
 import PropTypes from "prop-types";
 import { Button } from "components/ui/Button";
 import { useNavigate } from "react-router-dom";
 
+const FormField = (props) => {
+  return (
+    <div className="createTeam field">
+      <label className="createTeam label">{props.label}</label>
+      <input
+        className="createTeam input"
+        placeholder="enter here.."
+        value={props.value}
+        onChange={(e) => props.onChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+FormField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  type: PropTypes.string,
+};
+
 const CreateTeam = ({ isOpen, onClose, onCreateTeamClick }) => {
   if (!isOpen) return null;
-  const navigate = useNavigate();
+  const [teamName, setTeamName] = useState<string>(null);
 
   const createTeam = () => {
-    //team ID is received here from backend
-    const teamid = "12";
+    //team ID is received here from backend API API API
+    const teamid = "16";
     onCreateTeamClick(teamid);
   };
 
   return (
-    <div className="profileMenu-overlay" onClick={onClose}>
-      <div className="profileMenu-content" onClick={(e) => e.stopPropagation()}>
+    <div className="createTeam overlay" onClick={onClose}>
+      <div className="createTeam content" onClick={(e) => e.stopPropagation()}>
         <Button className="red-button" onClick={onClose}>
           Close
         </Button>
         <h2>Create Team</h2>
+        <FormField
+          label="Name"
+          value={teamName}
+          onChange={(tn: string) => setTeamName(tn)}
+        />
         <Button className="green-button" onClick={createTeam}>
           Accept
         </Button>
