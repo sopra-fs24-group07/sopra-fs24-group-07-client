a631a881cb83d187bed6d3cbb4c8fc7b2b770b49
timon.derboven <timon.derboven@uzh.ch>
2024-04-03 19:10:13 +0200

    added team members and time goal
    sopra-fs24-group07/sopra-fs24-group-07-client#21
    sopra-fs24-group07/sopra-fs24-group-07-client#22
    -added time goal input field (not functional)
    -added start group session button (not functional)
    -added team members list (functional when api is ready)

--- src/components/views/TeamDashboard.tsx
+++ src/components/views/TeamDashboard.tsx
@@ -1,13 +1,65 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { api, handleError } from "helpers/api";
+import { Form, useParams } from "react-router-dom";
 import "styles/views/TeamsOverview.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import "styles/views/TeamDashboard.scss";
 import TeamDashboardBox from "components/ui/TeamDashboardBox";
 import { Button } from "components/ui/Button";
+import PropTypes from "prop-types";
+
+const FormField = (props) => {
+  return (
+    <input
+      className="timeInput"
+      placeholder="Enter time Goal"
+      value={props.value}
+      onChange={(e) => props.onChange(e.target.value)}
+    />
+  );
+};
+
+FormField.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func,
+};
 
 const TeamDashboard = () => {
   const { teamId } = useParams();
+  const [time, setTime] = useState<string>(null);
+  let [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const fetchTeamMembers = async () => {
+      try {
+        let ID = teamId;
+        const response = await api.get(`/api/v1/teams/${ID}/users`);
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching teams users:", error);
+      }
+    };
+
+    fetchTeamMembers();
+
+    //TODO: remove fake TeamMember, when API call ready
+    const fakeTeamMembers = [
+      { id: 1, username: "Monti" },
+      { id: 2, username: "Basil" },
+      { id: 3, username: "Homie1" },
+      { id: 4, username: "Steve" },
+      { id: 5, username: "Lara" },
+      { id: 6, username: "Mario" },
+      { id: 7, username: "xXDogXx" },
+    ];
+    setUserData(fakeTeamMembers);
+    //remove until
+  }, []);
+
+  //TODO: add startGroupSession with time goal and tasks
+  function startGroupSession() {
+    throw new Error("Function not implemented.");
+  }
 
   return (
     <BaseContainer>
@@ -20,7 +72,13 @@ const TeamDashboard = () => {
             endRow={2}
             endColumn={2}
           >
-            Session Field
+            <div className="timeGoalBox">
+              Time Goal:
+              <FormField onChange={(t) => setTime(t)} />
+              <Button width="100%" onClick={() => startGroupSession()}>
+                Start Group Session
+              </Button>
+            </div>
           </TeamDashboardBox>
           <TeamDashboardBox
             startRow={2}
@@ -28,7 +86,14 @@ const TeamDashboard = () => {
             endRow={20}
             endColumn={2}
           >
-            Team Members
+            <div>
+              Team Members
+              <div>
+                {userData.map((member) => (
+                  <div key={member.id}>{member.username}</div>
+                ))}
+              </div>
+            </div>
           </TeamDashboardBox>
           <TeamDashboardBox
             startRow={1}
