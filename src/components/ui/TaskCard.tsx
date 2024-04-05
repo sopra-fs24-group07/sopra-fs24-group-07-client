4248a8a8b61d76442d343073dabf0799b362a63e
timon.derboven <timon.derboven@uzh.ch>
2024-04-05 12:44:43 +0200

    Task Update Endpoint call
    sopra-fs24-group07/sopra-fs24-group-07-client#6
    -added two api calls to update Task Status from TeamDashboard
    -handle the clicks on the move task buttons

--- src/components/ui/TaskCard.tsx
+++ src/components/ui/TaskCard.tsx
@@ -1,20 +1,78 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { api, handleError } from "helpers/api";
 import "../../styles/ui/TaskCard.scss";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { Button } from "./Button";
+import { useParams } from "react-router-dom";
 
 function TaskCard(props) {
   const { task, col } = props;
+  const { teamId } = useParams();
+  const taskId = task.id;
+
+  async function updateTaskStatusRight(task, col) {
+    if (col === "TODO") {
+      task.status = "IN_SESSION";
+    } else if (col === "IN_SESSION") {
+      task.status = "DONE";
+    }
+    try {
+      const requestBody = JSON.stringify(task);
+      const response = await api.put(
+        `/api/v1/teams/${teamId}/tasks/${taskId}`,
+        requestBody
+      );
+    } catch (error) {
+      console.log("Failed to Update Task");
+    }
+  }
+
+  async function updateTaskStatusLeft(task, col) {
+    if (col === "IN_SESSION") {
+      task.status = "TODO";
+    } else if (col === "DONE") {
+      task.status = "IN_SESSION";
+    }
+    try {
+      const requestBody = JSON.stringify(task);
+      const response = await api.put(
+        `/api/v1/teams/${teamId}/tasks/${taskId}`,
+        requestBody
+      );
+    } catch (error) {
+      console.log("Failed to Update Task");
+    }
+  }
 
   return (
     <div className="taskContainer">
-      {col === "DONE" && <Button className="goLeft"></Button>}
-      {col === "IN_SESSION" && <Button className="goLeft"></Button>}
+      {col === "DONE" && (
+        <Button
+          className="goLeft"
+          onClick={() => updateTaskStatusLeft(task, col)}
+        ></Button>
+      )}
+      {col === "IN_SESSION" && (
+        <Button
+          className="goLeft"
+          onClick={() => updateTaskStatusLeft(task, col)}
+        ></Button>
+      )}
       <div className="taskTitle">{task.title}</div>
-      {col === "TODO" && <Button className="goRight"></Button>}
-      {col === "IN_SESSION" && <Button className="goRight"></Button>}
+      {col === "TODO" && (
+        <Button
+          className="goRight"
+          onClick={() => updateTaskStatusRight(task, col)}
+        ></Button>
+      )}
+      {col === "IN_SESSION" && (
+        <Button
+          className="goRight"
+          onClick={() => updateTaskStatusRight(task, col)}
+        ></Button>
+      )}
     </div>
   );
 }
