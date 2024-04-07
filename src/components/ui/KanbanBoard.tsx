2b145ec8a9d429ab02412546ddd8d6527c64833c
timon.derboven <timon.derboven@uzh.ch>
2024-04-07 22:46:34 +0200

    comments in KanbanBoard
    sopra-fs24-group07/sopra-fs24-group-07-client#6
    -comments for clarity & docu

--- src/components/ui/KanbanBoard.tsx
+++ src/components/ui/KanbanBoard.tsx
@@ -5,12 +5,15 @@ import PropTypes from "prop-types";
 import { DndContext } from "@dnd-kit/core";
 
 function KanbanBoard(props) {
+  //define the three columns we have
   const columns = ["TODO", "IN_SESSION", "DONE"];
+  //get teams tasks from the props
   const { teamTasks } = props;
 
   return (
     <div className="board">
       <div className="cols">
+        {/* map the the Columns and give tasks to ColumnContainer */}
         {columns.map((col) => (
           <ColumnContainer
             key={col}
@@ -23,6 +26,7 @@ function KanbanBoard(props) {
   );
 }
 
+//check teamTasks to be array
 KanbanBoard.propTypes = {
   teamTasks: PropTypes.array,
 };
