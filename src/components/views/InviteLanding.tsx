c9418b9ba256935530e13cae838515d163d26d08
timon.derboven <timon.derboven@uzh.ch>
2024-04-15 14:30:30 +0200

    api call to join team
    sopra-fs24-group07/sopra-fs24-group-07-client#18
    -implemented and fixed the api call to join a team

--- src/components/views/InviteLanding.tsx
+++ src/components/views/InviteLanding.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { api, handleError } from "helpers/api";
 
 function InviteLanding() {
   const navigate = useNavigate();
@@ -8,10 +9,33 @@ function InviteLanding() {
   console.log(teamUUID);
 
   useEffect(() => {
-    if (!sessionStorage.getItem("token") && !sessionStorage.getItem("id")) {
+    if (!sessionStorage.getItem("token") || !sessionStorage.getItem("id")) {
       navigate("/login");
     } else {
-      const JoinTeam = async () => {};
+      const token = sessionStorage.getItem("token");
+      const userId = sessionStorage.getItem("id");
+
+      const JoinTeam = async () => {
+        try {
+          //const requestBody = JSON.stringify(teamUUID);
+          const requestBody = { teamUUID: teamUUID };
+          const response = await api.post(
+            `/api/v1/users/${userId}/teams`,
+            requestBody,
+            {
+              headers: { Authorization: `${token}` },
+            }
+          );
+          sessionStorage.removeItem("teamUUID"); //remove teamUUID
+          navigate("/teams"); //go to teams overview
+        } catch (error) {
+          console.error("Error joining team:", handleError(error));
+          //display some error
+          //remove teamUUID
+        }
+      };
+
+      JoinTeam();
     }
   }, []);
 
