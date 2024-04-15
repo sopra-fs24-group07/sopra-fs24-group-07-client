668ed57b246526bb82de29bcdff9d76542855398
timon.derboven <timon.derboven@uzh.ch>
2024-04-15 11:40:17 +0200

    Team Setting with Invite User
    sopra-fs24-group07/sopra-fs24-group-07-client#13
    sopra-fs24-group07/sopra-fs24-group-07-client#18
    -popup with team name and description
    -Invite user button that copies URL to clipboard
    -edit mode (not functional yet)

---
+++ src/components/popups/TeamSetting.tsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import "../../styles/popups/CreateTeam.scss";
+import { api, handleError } from "helpers/api";
+import PropTypes from "prop-types";
+import { Button } from "components/ui/Button";
+import { useNavigate, useParams } from "react-router-dom";
+
+const TeamSettings = ({ isOpen, onClose }) => {
+  const [editMode, setEditMode] = useState(false);
+  const [teamName, setTeamName] = useState();
+  const [teamDescription, setTeamDescription] = useState();
+  const [teamUUID, setTeamUUID] = useState();
+  const [copied, setCopied] = useState("");
+  const { teamId } = useParams();
+  const token = sessionStorage.getItem("token");
+  const userId = sessionStorage.getItem("id");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchUserTeam = async () => {
+      try {
+        const response = await api.get(`/api/v1/users/${userId}/teams`, {
+          headers: { Authorization: `${token}` },
+        });
+        const foundTeam = response.data.find(
+          (team) => team.teamId === parseInt(teamId)
+        );
+        setTeamName(foundTeam.name);
+        setTeamDescription(foundTeam.description);
+        setTeamUUID(foundTeam.teamUUID);
+      } catch (error) {
+        console.error("Error fetching user teams:", error);
+      }
+    };
+    fetchUserTeam();
+  }, []);
+
+  if (!isOpen) return null;
+
+  const ActivateEditMode = () => {
+    setEditMode(true);
+  };
+  const DeactivateEditMode = () => {
+    setEditMode(false);
+  };
+
+  const CopyInvitationLink = async () => {
+    try {
+      const URL = `productiviteam.co/invitation/${teamUUID}`;
+      await navigator.clipboard.writeText(URL);
+      setCopied("Copied to clipboard!");
+    } catch (error) {
+      setCopied("Failed to copy the UUID");
+    }
+  };
+
+  const doClose = () => {
+    setError("");
+    setCopied("");
+    DeactivateEditMode();
+    onClose();
+  };
+
+  return (
+    <div className="createTeam overlay" onClick={doClose}>
+      <div className="createTeam content" onClick={(e) => e.stopPropagation()}>
+        <Button className="red-button" onClick={doClose}>
+          Close
+        </Button>
+        <h2>Team Settings of</h2>
+        <input
+          value={teamName}
+          placeholder="Task Title..."
+          disabled={!editMode}
+        />
+        <h3>Team Description</h3>
+        <input
+          value={teamDescription}
+          placeholder="Task Description..."
+          disabled={!editMode}
+        />
+        <div>
+          <Button onClick={CopyInvitationLink}>Invite User</Button>
+          {copied && <p>{copied}</p>}
+        </div>
+
+        {error && <p>{error}</p>}
+
+        <div>
+          {!editMode && (
+            <Button className="green-button" onClick={ActivateEditMode}>
+              Edit
+            </Button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+TeamSettings.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  task: PropTypes.object.isRequired,
+};
+
+export default TeamSettings;
