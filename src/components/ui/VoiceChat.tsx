44e08d7aac02fdf7d8bc7162b87dcb9e96a15c30
timon.derboven <timon.derboven@uzh.ch>
2024-04-17 10:46:25 +0200

    added some comments
    sopra-fs24-group07/sopra-fs24-group-07-client#14

--- src/components/ui/VoiceChat.tsx
+++ src/components/ui/VoiceChat.tsx
@@ -4,13 +4,17 @@ import "../../styles/ui/VoiceChat.scss";
 import AgoraRTC from "agora-rtc-sdk-ng"; //RTC for voice transmitting
 import AgoraRTM from "agora-rtm-sdk"; //RTM for Channels, Users, etc.
 
-function VoiceChat() {
+const VoiceChat = () => {
+  //use Params for teamId
+  //localStorage for token
+  //localStorage for userId
+
   const APP_ID = "a55e8c2816d34eda92942fa9e808e843";
   const TOKEN = null;
 
   //IDs for identification, which we will use the userId for
   const [rtcUID, setRtcUID] = useState();
-  const [rtmUID, setRtmUID] = useState();
+  const [rtmUID, setRtmUID] = useState(); // .toString()
 
   //all the tasks, from which we will get our channels
   const [tasks, setTasks] = useState([]);
@@ -29,7 +33,11 @@ function VoiceChat() {
   const [rtcClient, setRtcClient] = useState(null);
   const [rtmClient, setRtmClient] = useState(null);
 
+  //API Call to get all Tasks, then filter for those IN_SESSION
+
+  //API Call to get my own Username
+
   return <div>VoiceChat</div>;
-}
+};
 
 export default VoiceChat;
