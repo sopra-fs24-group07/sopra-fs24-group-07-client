fa1237baa04da72e58b52c893e9f2eaea260f9fd
timon.derboven <timon.derboven@uzh.ch>
2024-04-17 10:37:07 +0200

    import Agora and create variables
    sopra-fs24-group07/sopra-fs24-group-07-client#14
    -npm install to get Agora RTC and RTM
    -all needed variables

--- src/components/ui/VoiceChat.tsx
+++ src/components/ui/VoiceChat.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/ui/VoiceChat.scss";
 
+import AgoraRTC from "agora-rtc-sdk-ng"; //RTC for voice transmitting
+import AgoraRTM from "agora-rtm-sdk"; //RTM for Channels, Users, etc.
+
 function VoiceChat() {
+  const APP_ID = "a55e8c2816d34eda92942fa9e808e843";
+  const TOKEN = null;
+
+  //IDs for identification, which we will use the userId for
+  const [rtcUID, setRtcUID] = useState();
+  const [rtmUID, setRtmUID] = useState();
+
+  //all the tasks, from which we will get our channels
+  const [tasks, setTasks] = useState([]);
+
+  //the name of the room (a single task in our case)
+  const [roomName, setRoomName] = useState("");
+
+  //the published channel, we use teamId + roomName to make individual channels
+  const [channel, setChannel] = useState("");
+
+  //local AudioTrack for myself and remoteAudioTracks of the others
+  const [localAudioTrack, setLocalAudioTrack] = useState(null);
+  const [remoteAudioTracks, setRemoteAudioTracks] = useState({});
+
+  //the Clients, which will be initialized later
+  const [rtcClient, setRtcClient] = useState(null);
+  const [rtmClient, setRtmClient] = useState(null);
+
   return <div>VoiceChat</div>;
 }
 
