832b0a887f348b5c8b582f3bd1091cd4cbb372f6
timon.derboven <timon.derboven@uzh.ch>
2024-04-18 18:12:51 +0200

    added console error
    sopra-fs24-group07/sopra-fs24-group-07-client#
    -Changes

--- src/components/popups/TeamSetting.tsx
+++ src/components/popups/TeamSetting.tsx
@@ -58,6 +58,7 @@ const TeamSettings = ({ isOpen, onClose }) => {
         if (error.response.status === 401) {
           setLeaveError("You are not authorized to do this");
         }
+        console.error(handleError(error));
       }
     };
 
