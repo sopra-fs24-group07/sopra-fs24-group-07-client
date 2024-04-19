d7de05fdcbef65a3084739406ddf72ec1b859c30
B1s9l <furrerbasil@gmail.com>
2024-04-19 20:41:13 +0200

    Precommit

--- src/components/ui/SessionTaskBoard.tsx
+++ src/components/ui/SessionTaskBoard.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../../styles/ui/SessionTaskBoard.scss";
 
-const SessionTaskBoard = ({ teamId, teamTasks, sessionStatus, checkedTasks, setCheckedTasks }) => {
+const SessionTaskBoard = ({
+  teamId,
+  teamTasks,
+  sessionStatus,
+  checkedTasks,
+  setCheckedTasks,
+}) => {
   if (!teamTasks) {
     return <p>No tasks currently in session!</p>;
   }
@@ -23,9 +29,9 @@ const SessionTaskBoard = ({ teamId, teamTasks, sessionStatus, checkedTasks, setC
       <h3>Session Tasks</h3>
       <div>
         <ul className="team-task-list">
-          {teamTasks.map(task => (
+          {teamTasks.map((task) => (
             <li key={task.taskId} style={{ listStyleType: "none" }}>
-              <label style={{ display: 'flex', alignItems: 'flex-start' }}>
+              <label style={{ display: "flex", alignItems: "flex-start" }}>
                 <input
                   type="checkbox"
                   checked={checkedTasks.has(task.taskId)}
@@ -33,7 +39,13 @@ const SessionTaskBoard = ({ teamId, teamTasks, sessionStatus, checkedTasks, setC
                   onChange={() => handleCheckboxChange(task)}
                   aria-disabled={sessionStatus === "off"}
                 />
-                <div style={{ display: "flex", flexDirection: "column", marginLeft: "5px" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginLeft: "5px",
+                  }}
+                >
                   <strong>{task.title}</strong>
                   <span>{task.description}</span>
                 </div>
@@ -47,16 +59,19 @@ const SessionTaskBoard = ({ teamId, teamTasks, sessionStatus, checkedTasks, setC
 };
 
 SessionTaskBoard.propTypes = {
-  teamTasks: PropTypes.arrayOf(PropTypes.shape({
-    taskId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-  })).isRequired,
+  teamTasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      taskId: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+        .isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   sessionStatus: PropTypes.string,
   teamId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   checkedTasks: PropTypes.instanceOf(Set).isRequired,
-  setCheckedTasks: PropTypes.func.isRequired
+  setCheckedTasks: PropTypes.func.isRequired,
 };
 
 export default SessionTaskBoard;
