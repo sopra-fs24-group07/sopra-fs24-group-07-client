8ef006ef6415892b811ef7e78f22ca18e9e48303
timon.derboven <timon.derboven@uzh.ch>
2024-04-19 02:05:21 +0200

    get comments api call
    sopra-fs24-group07/sopra-fs24-group-07-client/issues/10
    -get all the comments
    -error handling

--- src/components/ui/Comments.tsx
+++ src/components/ui/Comments.tsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 import "../../styles/ui/Comments.scss";
-import { Button } from './Button';
-import { useParams } from 'react-router-dom';
+import { Button } from "./Button";
+import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import { api, handleError } from "helpers/api";
 
-const FormField = ({value, onChange, error }) => {
+const FormField = ({ value, onChange, error }) => {
   return (
     <div>
       <input
@@ -26,42 +26,72 @@ FormField.propTypes = {
   error: PropTypes.string,
 };
 
-
 const Comments = (props) => {
-  const {teamId} = useParams();
-  const {taskId} = props;
+  const { teamId } = useParams();
+  const { taskId } = props;
   const token = localStorage.getItem("token");
   const [comment, setComment] = useState("");
   const [error, setError] = useState("");
+  const [allComments, setAllComments] = useState([]);
 
   const createComment = async () => {
     try {
-      const requestBody = JSON.stringify({text: comment});
-      const response = await api.post(`/teams/${teamId}/tasks/${taskId}/comments`, requestBody, {
-        headers: {
-          Authorization: `${token}`,
-        },
-      });
-      console.log(response)
-    }catch (error) {
-    console.error("Error creating comment:", handleError(error));
-    setError("Sending the comment failed")
-    } 
-  }
+      const requestBody = JSON.stringify({ text: comment });
+      const response = await api.post(
+        `/teams/${teamId}/tasks/${taskId}/comments`,
+        requestBody,
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Error creating comment:", handleError(error));
+      setError("Could not create Comment");
+    }
+  };
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      try {
+        const response = await api.get(
+          `/teams/${teamId}/tasks/${taskId}/comments`,
+          {
+            headers: {
+              Authorization: `${token}`,
+            },
+          }
+        );
+        console.log(response.data);
+        setAllComments(response.data);
+      } catch (error) {
+        console.error("Error fetching comments:", handleError(error));
+        setError("Could not load comments");
+      }
+    };
+    fetchComments();
+  }, []);
+
   return (
     <div>
-      <FormField
-        value={comment}
-        onChange={setComment}
-        error={error}
-      />
-      <Button disabled={!comment} className="green-button" onClick={createComment}>Submit</Button>
+      <div>
+        <FormField value={comment} onChange={setComment} error={error} />
+        <Button
+          disabled={!comment}
+          className="green-button"
+          onClick={createComment}
+        >
+          Submit
+        </Button>
+      </div>
+      <div></div>
     </div>
-  )
-  
-}
+  );
+};
 Comments.propTypes = {
-  taskId: PropTypes.number
-}
+  taskId: PropTypes.number,
+};
 
 export default Comments;
