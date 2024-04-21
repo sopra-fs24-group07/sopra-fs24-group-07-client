8c38054e6753c5a33b4ec158a1a5e819f610776c
timon.derboven <timon.derboven@uzh.ch>
2024-04-21 13:18:13 +0200

    moved spinner to different component
    sopra-fs24-group07/sopra-fs24-group-07-client/issues/14

--- src/components/ui/VoiceChat.tsx
+++ src/components/ui/VoiceChat.tsx
@@ -7,34 +7,7 @@ import AgoraRTC from "agora-rtc-sdk-ng"; //RTC for voice transmitting
 import AgoraRTM from "agora-rtm-sdk"; //RTM for Channels, Users, etc.
 import BaseContainer from "./BaseContainer";
 import { Button } from "./Button";
-
-const Spinner = () => (
-  <div
-    style={{
-      position: "fixed",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent black background
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      zIndex: 1000, // Ensures it is on top of other elements
-    }}
-  >
-    <div
-      style={{
-        width: "40px",
-        height: "40px",
-        border: "4px solid #f3f3f3",
-        borderTop: "4px solid #3498db",
-        borderRadius: "50%",
-        animation: "spin 1s linear infinite",
-      }}
-    />
-  </div>
-);
+import { Spinner } from "./Spinner";
 
 function VoiceChat() {
   const APP_ID = "a55e8c2816d34eda92942fa9e808e843";
