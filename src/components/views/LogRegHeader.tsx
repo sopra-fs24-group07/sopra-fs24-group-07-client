4e95ddeb08ae87e77722860b3b4c4e66bd9c89fa
timon.derboven <timon.derboven@uzh.ch>
2024-04-21 14:51:07 +0200

    add styling to register
    sopra-fs24-group07/sopra-fs24-group-07-client/issues/
    -same changes as for login
    -also include a new header

---
+++ src/components/views/LogRegHeader.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "../../styles/views/LogRegHeader.scss";
+
+const LogRegHeader = () => {
+  return (
+    <div className="header container">
+      <h1 className="header title">
+        PRODUCTIVI<span className="header titlelarge">T</span>EAM
+      </h1>
+    </div>
+  );
+};
+
+export default LogRegHeader;
