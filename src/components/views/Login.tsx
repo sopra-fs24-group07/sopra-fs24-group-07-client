d99674bfab01d141a5e12347fad9652aee48ca9d
timon.derboven <timon.derboven@uzh.ch>
2024-04-21 13:46:58 +0200

    get Spinner from different component

--- src/components/views/Login.tsx
+++ src/components/views/Login.tsx
@@ -6,6 +6,7 @@ import User from "models/User";
 import { Button } from "components/ui/Button";
 import "styles/views/Login.scss";
 import BaseContainer from "components/ui/BaseContainer";
+import { Spinner } from "components/ui/Spinner";
 
 //NEW FORMFIELD: WITHOUT CODE REPETITION
 const FormField = ({ label, value, onChange, type = "text" }) => (
@@ -31,34 +32,6 @@ FormField.propTypes = {
   onChange: PropTypes.func.isRequired,
   type: PropTypes.string,
 };
-
-const Spinner = () => (
-  <div
-    style={{
-      position: "fixed",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      backgroundColor: "rgba(0, 0, 0, 0.5)", // Semi-transparent black background
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      zIndex: 1000, // Ensures it is on top of other elements
-    }}
-  >
-    <div
-      style={{
-        width: "40px",
-        height: "40px",
-        border: "4px solid #f3f3f3",
-        borderTop: "4px solid #3498db",
-        borderRadius: "50%",
-        animation: "spin 1s linear infinite",
-      }}
-    />
-  </div>
-);
 
 const Login = () => {
   const navigate = useNavigate();
