f4080edf4a053a0116eaeb7cd1a662035f988b57
timon.derboven <timon.derboven@uzh.ch>
2024-05-07 21:33:34 +0200

    changes for color of Icon
    sopra-fs24-group07/sopra-fs24-group-07-client/issues/68
    -can now change color of each icon (for now red/green)

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -3,48 +3,48 @@ import PropTypes from "prop-types";
 import "../../styles/ui/IconButton.scss";
 
 const IconButton = (props) => {
-    const {
-      hoverIcon: HoverIcon,
-      icon: Icon,
-      toggled: toggled = false,
-      ...rest
-    } = props;
-    const [hover, setHover] = useState(false);
-  
-    return (
-      <button
-        {...rest}
-        style={{
-          width: props.width,
-          background: "none",
-          border: "none",
-          padding: 0,
-          color: "inherit",
-          cursor: "pointer",
-          ...props.style,
-        }}
-        className={`icon-button ${props.className}`}
-        onMouseEnter={() => setHover(true || toggled)}
-        onMouseLeave={() => setHover(false || toggled)}
-      >
-        {hover && HoverIcon ? (
-          <HoverIcon size={20} className={"icon-button-icon"} />
-        ) : (
-          <Icon size={20} className={"icon-button-icon"} />
-        )}
-        {props.children}
-      </button>
-    );
-  };
+  const {
+    hoverIcon: HoverIcon,
+    icon: Icon,
+    toggled: toggled = false,
+    ...rest
+  } = props;
+  const [hover, setHover] = useState(false);
 
-  IconButton.propTypes = {
-    hoverIcon: PropTypes.elementType,
-    icon: PropTypes.elementType,
-    toggled: PropTypes.bool,
-    width: PropTypes.string,
-    style: PropTypes.object,
-    className: PropTypes.string,
-    children: PropTypes.node,
+  return (
+    <button
+      {...rest}
+      style={{
+        width: props.width,
+        background: "none",
+        border: "none",
+        padding: 0,
+        color: "inherit",
+        cursor: "pointer",
+        ...props.style,
+      }}
+      className={`icon-button ${props.className}`}
+      onMouseEnter={() => setHover(true || toggled)}
+      onMouseLeave={() => setHover(false || toggled)}
+    >
+      {hover && HoverIcon ? (
+        <HoverIcon size={20} className={`${props.className}`} />
+      ) : (
+        <Icon size={20} className={`${props.className}`} />
+      )}
+      {props.children}
+    </button>
+  );
+};
+
+IconButton.propTypes = {
+  hoverIcon: PropTypes.elementType,
+  icon: PropTypes.elementType,
+  toggled: PropTypes.bool,
+  width: PropTypes.string,
+  style: PropTypes.object,
+  className: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default IconButton;
