6ef2c5fd39e492854dbd6cd0125fa736863b0aec
Alihan26 <alihan.karatasli@hotmail.com>
2024-05-15 17:47:12 +0200

    Added descriptions annd major restyling
    Sopra-fs24-group07/sopra-fs24-group-07-client#146

--- src/components/popups/Tutorial.tsx
+++ src/components/popups/Tutorial.tsx
@@ -18,12 +18,30 @@ const TutorialPopup = ({ isOpen, onClose }) => {
         {overviewTutorial ? (
             <div>
             <h1>Team Overview Tutorial</h1>
+            <h3>Create Team</h3>
+            <p>Click on the Create Team button to create your first team! After setting a team name, you can either write your own description, or click on the orange wand button to automatically generate a team description.</p>
             </div>
         ) : (
             <div>
             <h1>Team Dashboard Tutorial</h1>
-            <h3>Kanbanboard</h3>
-            <p>Text copz paste</p>
+            <h3>Kanban Board</h3>
+            <p>A Kanban board is a visual project management tool that helps to optimize the flow of work among a team. It is divided into different columns, each representing a different stage of the workflow.  
+            The board provides a visual representation of the work in progress and helps teams to identify bottlenecks and manage workloads effectively.
+            You can drag and drop your tasks into one of the following three columns. 
+            TODO column: List of all the tasks you haven&#39;t started yet
+            NEXT SESSION column: List of all the tasks you want to work on in the next session
+            DONE column: List of all your finished tasks</p>
+            <h3>Session Start</h3>
+            <p>You can set a time goal  and start the group session by clicking on the button underneath the time goal. 
+            The tasks that has been dragged to Next Session in the Kanban board will be automatically migrated to the session taskboard.</p>
+            <h3>Voice Chat</h3>
+            <p>You can join the VoiceChat by clicking on one of the channels. You will always have a main channels, and all the other channels are the breakout rooms of the active tasks. 
+            You can always mute yourself by clicking on the mute button.</p>
+            <h3>Sessions History</h3>
+            <p>Click on the Session History Button to get a list of your session stats.</p>
+            <h3>Comments</h3>
+            <p>By clicking on the task inside the Kanban board, you can add comments for the specific task.</p>
+            
             </div>
         )}
           <Button onClick={onClose}>Got it!</Button>  
