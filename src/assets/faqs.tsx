88ce6becc3cfcf297f12cfc8bc68237f7c5dc352
B1s9l <furrerbasil@gmail.com>
2024-05-17 18:20:29 +0200

    Updated the faqs

--- src/assets/faqs.tsx
+++ src/assets/faqs.tsx
@@ -1,7 +1,7 @@
 export const faqList = [
   {
     question: "How do I restart the tutorial?",
-    answer: "Click the tutorial button in your profile again.",
+    answer: "Click the tutorial button in the FAQS page, you can find the question mark button on the bottom left.",
   },
   {
     question: "Where can I find my teams?",
