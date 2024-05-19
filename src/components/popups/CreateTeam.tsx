82283a5f2748e6e068e60c429b2306eb9687b0fb
5v3nn <sven.greuter@protonmail.ch>
2024-05-19 22:55:42 +0200

    add title to ai button fo more understandability

--- src/components/popups/CreateTeam.tsx
+++ src/components/popups/CreateTeam.tsx
@@ -181,6 +181,7 @@ const CreateTeam = ({ isOpen, onClose, onCreateTeamClick }) => {
               alignSelf: "flex-end",
             }}
             disabled={isButtonDisabled}
+            title={"Generate description with an poetic AI"}
           />
         </FormField>
         {getAllErrorMessages().map((error, index) => (
